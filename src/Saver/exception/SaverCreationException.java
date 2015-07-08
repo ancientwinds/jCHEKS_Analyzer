6fa7236b280530a7675e138b563d0e24209113fa
Michael Roussel <rousselm4@gmail.com>
2015-07-08 09:07:27 -0400

    Add db storage classes.

---
+++ src/Saver/exception/SaverCreationException.java
@@ -0,0 +1,12 @@
+package Saver.exception;
+
+/**
+ *
+ * @author Michael Roussel <rousselm4@gmail.com>
+ */
+public class SaverCreationException extends Exception {
+
+    public SaverCreationException() {
+    }
+    
+}
---
+++ src/Saver/exception/SavingException.java
@@ -0,0 +1,12 @@
+package Saver.exception;
+
+/**
+ *
+ * @author Michael Roussel <rousselm4@gmail.com>
+ */
+public class SavingException extends Exception {
+
+    public SavingException() {
+    }
+    
+}
