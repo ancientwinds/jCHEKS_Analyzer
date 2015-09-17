7a67225868838073bf9f5c64aa788f5bf61f49f5
Michael Roussel <rousselm4@gmail.com>
2015-09-17 17:20:48 -0400

    Add details in comment about implementation of NIST 7 test.

--- src/cheksAnalyse/nistTest/TestNonOverlappingTemplateMatchingNIST7.java
+++ src/cheksAnalyse/nistTest/TestNonOverlappingTemplateMatchingNIST7.java
@@ -24,6 +24,28 @@ public class TestNonOverlappingTemplateMatchingNIST7 extends AbstractNistTest{
          * This method should change pValue attribute of the result.
          * this.pValue = result;
          */
+        
+        /*
+        From http://jrandtest.sourceforge.net
+        The focus of this test is the number of occurrences of pre-defined target substrings. 
+        The purpose of this test is to reject sequences that exhibit too many occurrences of a 
+        given non-periodic (aperiodic) pattern. For this test and for the Overlapping Template 
+        Matching test, an m-bit window is used to search for a specific m-bit pattern. If the 
+        pattern is not found, the window slides one bit position. For this test, when the pattern 
+        is found, the window is reset to the bit after the found pattern, and the search resumes.
+        
+        Python code
+            n = len(binin)
+            m = len(mat)
+            M = n/num
+            blocks = [binin[xs*M:M+xs*M:] for xs in xrange(n/M)]
+            counts = [xx.count(mat) for xx in blocks]
+            avg = 1.0 * (M-m+1)/2 ** m
+            var = M*(2**-m -(2*m-1)*2**(-2*m))
+            chisqr = reduce(su, [(xs - avg) ** 2 for xs in counts]) / var
+            pval = spc.gammaincc(1.0 * len(blocks) / 2, chisqr / 2)
+            return pval
+        */
     }
     
     @Override
