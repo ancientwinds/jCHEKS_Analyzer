0d9614203c7dee27a3e06d006da3144fa11e3d87
Jean-François Cloutier <ancientwinds@gmail.com>
2015-09-22 15:50:41 -0400

    Confluent hypergeometric function
    Added to be used in NIST8 test

---
+++ src/Utils/Polynomial.java
@@ -0,0 +1,51 @@
+/*
+ * File imported from edu.missouristate.mote.statistics
+ * GitHub: https://github.com/larmer01/Mote/blob/master/src/edu/missouristate/mote/statistics/Polynomial.java
+ */
+
+package Utils;
+
+public final class Polynomial {
+
+    // *************************************************************************
+    // CONSTRUCTORS
+    // *************************************************************************
+    /**
+     * Initialize a new instance of a Polynomial.
+     */
+    private Polynomial() {
+    }
+
+    // *************************************************************************
+    // PUBLIC METHODS
+    // *************************************************************************
+    /**
+     * Evaluate a polynomial.
+     *
+     * @param xValue x value
+     * @param coeffs coefficients in reverse order
+     * @return evaluated polynomial
+     */
+    public static double eval(final double xValue, final double[] coeffs) {
+        double result = coeffs[0];
+        for (int index = 1; index < coeffs.length; index++) {
+            result = result * xValue + coeffs[index];
+        }
+        return result;
+    }
+
+    /**
+     * Evaluate a polynomial where the coefficient of x^n is assumed to be 1.
+     *
+     * @param xValue x value
+     * @param coeffs coefficients in reverse order
+     * @return evaluated polynomial
+     */
+    public static double evalOne(final double xValue, final double[] coeffs) {
+        double result = 1;
+        for (int index = 0; index < coeffs.length; index++) {
+            result = result * xValue + coeffs[index];
+        }
+        return result;
+    }    
+}
