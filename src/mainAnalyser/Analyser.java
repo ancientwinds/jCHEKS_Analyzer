e54cf5ac7624609331bd94dde22d7df0b52c5a76
Michael Roussel <rousselm4@gmail.com>
2015-10-10 08:14:51 -0400

    Change test to do.

--- src/mainAnalyser/Analyser.java
+++ src/mainAnalyser/Analyser.java
@@ -20,7 +20,10 @@ public class Analyser {
         types.add(AnalyserType.OCCURENCE);
         types.add(AnalyserType.VARIATION);
         */
+        types.add(AnalyserType.NIST_7);
         types.add(AnalyserType.NIST_8);
+        types.add(AnalyserType.NIST_11);
+        types.add(AnalyserType.NIST_12);
         /*types.add(AnalyserType.NIST_2);
         types.add(AnalyserType.NIST_3);
         types.add(AnalyserType.NIST_4);
