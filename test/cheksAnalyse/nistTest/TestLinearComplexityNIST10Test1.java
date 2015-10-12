3b336cf77b93fbf69f061ca5be84ca4aab30d906
Michael Roussel <rousselm4@gmail.com>
2015-10-12 13:44:48 -0400

    Create unit test Nist 10 and clean Nist test 10

---
+++ test/cheksAnalyse/nistTest/TestLinearComplexityNIST10Test1.java
@@ -0,0 +1,63 @@
+package cheksAnalyse.nistTest;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import utils.TestDataLoader;
+
+/**
+ *
+ * @author Michael Roussel <rousselm4@gmail.com>
+ */
+public class TestLinearComplexityNIST10Test1 {
+    
+   /* 
+    *    Testing Linear Complexity Test  - Results from C implementation (sts-2.1.2)
+    *    pi              p expected =  0.063927 (1004882 bits)
+    *    e               p expected =  0.828715 (1004882 bits)
+    *    sqrt2           p expected =  0.320415 (1004883 bits)
+    *    sqrt3           p expected =  0.622584 (1004883 bits)
+    */
+    
+    private AbstractNistTest nistTester;
+    private MockNISTSaver saver;
+    public static double DOUBLE_PRECISION = 0.000001;
+    
+    @Before
+    public void setup() throws Exception{
+        nistTester = new TestLinearComplexityNIST10(new ChaoticSystemStub());
+        saver = new MockNISTSaver();
+    }
+    
+    @Test
+    public void checkWithPiInput() throws Exception {
+        TestDataLoader loader = new TestDataLoader("TestData/pi");
+        nistTester.executeTest(loader.getDataAsBooleanArray());
+        nistTester.saveResult(saver);
+        assertEquals(0.063927, saver.getPValue(), DOUBLE_PRECISION);
+    }
+    
+    @Test
+    public void checkWithEInput() throws Exception {
+        TestDataLoader loader = new TestDataLoader("TestData/e");
+        nistTester.executeTest(loader.getDataAsBooleanArray());
+        nistTester.saveResult(saver);
+        assertEquals(0.828715, saver.getPValue(), DOUBLE_PRECISION);
+    }
+    
+    @Test
+    public void checkWithSqrt2Input() throws Exception {
+        TestDataLoader loader = new TestDataLoader("TestData/sqrt2");
+        nistTester.executeTest(loader.getDataAsBooleanArray());
+        nistTester.saveResult(saver);
+        assertEquals(0.320415, saver.getPValue(), DOUBLE_PRECISION);
+    }
+    
+    @Test
+    public void checkWithSqrt3Input() throws Exception {
+        TestDataLoader loader = new TestDataLoader("TestData/sqrt3");
+        nistTester.executeTest(loader.getDataAsBooleanArray());
+        nistTester.saveResult(saver);
+        assertEquals(0.622584, saver.getPValue(), DOUBLE_PRECISION);
+    }
+
+}
